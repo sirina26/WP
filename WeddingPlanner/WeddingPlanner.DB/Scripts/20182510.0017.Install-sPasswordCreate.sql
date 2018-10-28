b02ef8aa99a222335ef7692ed776b2aca031834b
sirine <sassi@intechinfo.fr>
2018-10-28 21:52:30 +0100

    creat Password

---
+++ WeddingPlanner/WeddingPlanner.DB/Scripts/20182510.0017.Install-sPasswordCreate.sql
@@ -0,0 +1,24 @@
+create procedure weddingplanner.sPasswordUserCreate
+(
+    @Email    nvarchar(64),
+    @Password varbinary(128),
+	@UserId   int out
+)
+as
+begin
+	set transaction isolation level serializable;
+	begin tran;
+
+	if exists(select * from weddingplanner.tUsers u where u.Email = @Email)
+	begin
+		rollback;
+		return 1;
+	end;
+
+    insert into weddingplanner.tUsers(Email) values(@Email);
+    select @UserId = scope_identity();
+    insert into weddingplanner.tPasswordUsers(UserId,  [Password])
+                           values(@userId, @Password);
+	commit;
+    return 0;
+end;
