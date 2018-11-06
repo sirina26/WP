f28f369f4de463ecc54130032e9f5b26c885aa4b
sirine <sassi@intechinfo.fr>
2018-11-06 15:42:41 +0100

    color

--- WeddingPlanner/WeddingPlanner.Spa/src/components/Home.vue
+++ WeddingPlanner/WeddingPlanner.Spa/src/components/Home.vue
@@ -1,12 +1,12 @@
 <template>
-    <section class="jumbotron text-center">
+    <section class="jumbotron text-center" style="background-color: #ffc1f3;opacity:0.8;">
         <div class="container">
             <h1 class="jumbotron-heading">Bienvenue sur Wedding Planner</h1>
             <p class="lead text-muted">Cette application est réalisée à l'aide du framework Vue 2.</p>
             <p>
-                <router-link class="btn btn-secondary my-2" to="/playground">Playground</router-link>                   
-                <br><router-link class="btn btn-secondary my-2" to="/playground">Connecter Tant que</router-link>
-                <br><router-link class="btn btn-secondary my-2" to="/playground">Playground</router-link>
+                <router-link class="btn btn-danger" to="/playground">Playground</router-link>                   
+                <br><br><router-link class="btn btn-danger" to="/playground">Connecter Tant que</router-link>
+                <br><br><router-link class="btn btn-danger" to="/playground">Playground</router-link>
             </p>
         </div>
     </section>
